46ef0938f168e69f59cc1b2d5f2947f2e6c14dad
Grant Mullinax <grant.mullinax@siemens.com>
2017-10-07 21:53:53 -0400

    added url regex checking

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_pymongo import PyMongo
 import random
+import re
 
 app = Flask(__name__)
 
@@ -11,25 +12,35 @@ app.config['MONGO_URI'] = 'mongodb://grant:chrisisstupid123@ds115045.mlab.com:15
 mongo = PyMongo(app)
 api = Api(app)
 
+urlRegex = re.compile('https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)')
+
 clientId = random.randint(0,999999)
 
 class Location(Resource):
     def post(self, x, y):
         if not x.isdigit() or not y.isdigit():
-            return "invalid coordinates", 400
+            return "Invalid request: invalid coordinates", 400
 
         locations = mongo.db.locations
 
         data = request.get_json(force=True)
 
+        if not 'url' in data:
+            return "null url", 400
+
+        url = data['url']
+
+        if not urlRegex.match(url):
+            return "Invalid request: null url", 400
+
         location = {'_id': str(x)+','+str(y),
-                'url': data['url']}
+                'url': url}
 
         locations.insert(location)
 
     def get(self, x, y):
         if not x.isdigit() or not y.isdigit():
-            return "invalid coordinates", 400
+            return "Invalid request: invalid coordinates", 400
 
         locations = mongo.db.locations
         location = locations.find_one({'_id': str(x)+','+str(y)})
